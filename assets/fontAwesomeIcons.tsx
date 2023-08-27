4f41e013ae23f7b4db3a740cf84d12e0469aaf01
Shannon Yazdani <shannonyazdani@gmail.com>
2023-08-27 16:22:51 -0400

    new assets

--- assets/fontAwesomeIcons.tsx
+++ assets/fontAwesomeIcons.tsx
@@ -1,6 +1,8 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
 import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faCircleCheck);
 library.add(faCircleExclamation);
+library.add(faArrowUpRightFromSquare);
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,9 @@ import Heading from "../components/Heading";
 import ThemeSwitcher from "../components/ThemeSwitcher";
 import logo from "../assets/logo.png";
 import Image from "next/image";
+import githubLight from 'assets/github-mark.svg'
+import githubDark from 'assets/github-mark-white.svg'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Home: NextPage = () => {
   return (
@@ -29,16 +32,25 @@ const Home: NextPage = () => {
           <Heading />
           <FileUpload />
         </section>
-        <footer className="bg-bg-primary dark:bg-bg-primary-dark flex justify-center items-center">
-          <p className="text-primary dark:text-primary-dark">Onchain Library powered by </p>
-          <a
-            href="https://layerr.xyz"
-            className="text-primary dark:text-primary-dark ml-1"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <Image width={80} src={logo} alt="layerr icon"></Image>
+        <footer className="bg-bg-primary gap-2 dark:bg-bg-primary-dark flex flex-col justify-center items-center">
+          <a target="_blank" rel="noreferrer" href="https://github.com/Layerrxyz/OnchainLibrary" className="text-primary dark:text-primary-dark flex gap-2 items-center">
+            <Image src={githubLight} className="dark:hidden h-6 w-6" alt="github logo" width={20} height={20} />
+            <Image src={githubDark} className="hidden dark:inline-block h-6 w-6" alt="github logo" width={15} height={15} />
+            <p className="w-[200px] sm:w-auto text-starttext-primary dark:text-primary-dark">View public repository for contracts on GitHub</p>
+            <FontAwesomeIcon icon={['fas', 'arrow-up-right-from-square']} />
           </a>
+
+          <div className="flex items-center mb-4 ">
+            <p className="text-primary dark:text-primary-dark">Onchain Library powered by </p>
+            <a
+              href="https://layerr.xyz"
+              className="text-primary dark:text-primary-dark ml-1"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <Image width={80} src={logo} alt="layerr icon"></Image>
+            </a>
+          </div>
         </footer>
       </main>
     </div>
